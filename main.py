3f99f30366aca1eb95c081c0681d5d5f778853c7
Colin Cron <colint.cron@gmail.com>
2020-11-22 22:34:25 -0500

    Added additional functionality
    Now scrapes for subreddits, users, and topics

--- main.py
+++ main.py
@@ -3,18 +3,50 @@ import re
 from bs4 import BeautifulSoup
 
 sub_pattern = re.compile("\\/r\/([a-zA-Z0-9-_]*)\/$")
+topic_pattern = re.compile("\\/t\/([a-zA-Z0-9-_]*)\/$")
+user_pattern = re.compile("\\/user\/([a-zA-Z0-9-_]*)\/$")
 link_list = []
 done_list = []
 garbage_list = []
 subs_list = []
+users_list = []
+topics_list = []
+base_url = "http://www.reddit.com"
 
 def write_to_file(href):
-    with open("test.txt", "a") as myfile:
-        print("File opened")
-        myfile.write(href + "\n")
-        print("Written to output file")
-        myfile.close()
-        print("File closed")
+    if href.startswith('/r/'):
+        print("Write function: sub")
+        with open("subs.txt", "a") as myfile:
+            print("File opened")
+            full_url = base_url + href + "\n"
+            print("FULL URL TEST:::::::: %s " % full_url)
+            myfile.write(full_url)
+            link_list.append(full_url)
+            print("Written to output file")
+            myfile.close()
+            print("File closed")
+    if href.startswith('/user/'):
+        print("Write function: user")
+        with open("users.txt", "a") as myfile:
+            print("File opened")
+            full_url = base_url + href + "\n"
+            print("FULL URL TEST:::::::: %s " % full_url)
+            myfile.write(full_url)
+            link_list.append(full_url)
+            print("Written to output file")
+            myfile.close()
+            print("File closed")
+    if href.startswith('/t/'):
+        print("Write function: topic")
+        with open("topics.txt", "a") as myfile:
+            print("File opened")
+            full_url = base_url + href + "\n"
+            print("FULL URL TEST:::::::: %s " % full_url)
+            myfile.write(full_url)
+            link_list.append(full_url)
+            print("Written to output file")
+            myfile.close()
+            print("File closed")
 
 def get_site(url):
     print("Adding site %s to link_list" % url)
@@ -48,17 +80,32 @@ def get_links(r):
             else:
                 print('accounted for: %s ' % href )
         else:
-            print("Garbage link: %s " % href)
-            if href.startswith('/r/') and sub_pattern.match(href) and href not in subs_list:
-                print("Subreddit found! %s " % href)
-                print("Appending to subs_list... %s" % href)
-                subs_list.append(href)
-                print("Writing to file... %s" % href)
-                write_to_file(href)
+            print("Possible garbage link: %s " % href)
+            print("Deciding if it's a subreddit, a user, or a topic")
+            if href.startswith('/r/') and href not in subs_list:
+                if sub_pattern.match(href):
+                    print("SUBREDDIT found! %s " % href)
+                    print("Appending to subs_list... %s" % href)
+                    subs_list.append(href)
+                    print("Writing to file... %s" % href)
+                    write_to_file(href)
+            if href.startswith('/user/') and href not in users_list:
+                if user_pattern.match(href):
+                    print("USER found! %s " % href)
+                    print("Appending to the users_list... %s " % href)
+                    users_list.append(href)
+                    print("Writing to file... %s " % href)
+                    write_to_file(href)
+            if href.startswith('/t/') and href not in topics_list:
+                if topic_pattern.match(href):
+                    print("TOPIC found! %s " % href)
+                    print("Appending to the topics_list... %s " % href)
+                    topics_list.append(href)
+                    print("Writing to file... %s " % href)
+                    write_to_file(href)
+                    
             garbage_list.append(href)
 
 if __name__ == "__main__":
-    url = "http://www.reddit.com"
-    
-    get_site(url)
-    print(link_list)
+    get_site(base_url)
+    #print(link_list)
